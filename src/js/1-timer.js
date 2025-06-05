5fd2b484d8268ac5d03862cb4a149084058fb2fb
Oleksandr Honchar <figules@gmail.com>
2025-06-05 10:29:49 +0200

    fix: update timer logic to disable date picker and improve time formatting

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -28,8 +28,6 @@ datePicker.addEventListener('input', () => {
   }
 });
 startButton.addEventListener('click', () => {
-  startButton.disabled = true;
-  datePicker.disabled = true;
   if (timerId) {
     clearInterval(timerId);
   }
@@ -41,20 +39,33 @@ startButton.addEventListener('click', () => {
       timerDisplay.textContent = 'Time is up!';
       return;
     }
-    const days = String(
-      Math.floor(timeDifference / (1000 * 60 * 60 * 24))
-    ).padStart(2, '0');
-    const hours = String(
-      Math.floor((timeDifference / (1000 * 60 * 60)) % 24)
-    ).padStart(2, '0');
-    const minutes = String(
-      Math.floor((timeDifference / (1000 * 60)) % 60)
-    ).padStart(2, '0');
-    const seconds = String(Math.floor((timeDifference / 1000) % 60)).padStart(
-      2,
-      '0'
-    );
+    function convertMs(ms) {
+      // Number of milliseconds per unit of time
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
 
-    timerDisplay.textContent = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+      // Remaining days
+      const days = Math.floor(ms / day);
+      // Remaining hours
+      const hours = Math.floor((ms % day) / hour);
+      // Remaining minutes
+      const minutes = Math.floor(((ms % day) % hour) / minute);
+      // Remaining seconds
+      const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+      return { days, hours, minutes, seconds };
+    }
+
+    console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+    console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+    console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+    const { days, hours, minutes, seconds } = convertMs(timeDifference);
+    timerDisplay.textContent = `${String(days).padStart(2, '0')} : ${String(
+      hours
+    ).padStart(2, '0')} : ${String(minutes).padStart(2, '0')} : ${String(
+      seconds
+    ).padStart(2, '0')}`;
   }, 1000);
 });
