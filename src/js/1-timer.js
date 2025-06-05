23138266ec10e6df98f39f2074ccd94b2ae3363f
Oleksandr Honchar <figules@gmail.com>
2025-06-05 10:37:59 +0200

    fix: add iziToast for enhanced notification support

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,5 +1,7 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const datePicker = document.querySelector('#datetime-picker');
 const startButton = document.querySelector('button[data-start]');
